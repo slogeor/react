0593d1540792c6568df055ddd2f2a1ea703cecfe
xiao.geng <xiao.geng@wormpex.com>
2018-01-09 14:59:54 +0800

    add Redux demo

--- example/Redux/redux-demo/src/index.js
+++ example/Redux/redux-demo/src/index.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux'
 
+// action 函数
 function todo(state = [], action) {
-  console.log('todo')
+  console.log('todo action:', action)
   switch (action.type) {
     case 'ADD_TODO':
       state.push(action.text)
@@ -11,17 +12,25 @@ function todo(state = [], action) {
   }
 }
 
-let store = createStore(todo, [1111]);
+// 初始化store
+let store = createStore(todo, ['default store']);
 
+// 监听 store 的变化
 store.subscribe(() =>
-  console.log(store.getState())
+  console.log('store subscribe',store.getState())
 )
 
 // 打印初始状态
 console.log('default store:',store.getState())
 
+// 触发action
 store.dispatch({
   type: 'ADD_TODO',
-  text: 'Learn about actions V2'
+  text: 'Learn React'
 });
-console.log('update store:',store.getState())
+
+// 触发action
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Learn Redux'
+});
